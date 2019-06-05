3b046e984d921d25478b51a63f90326d096a8376
jzxb <qq807572809@live.com>
2019-06-05 18:35:14 +0800

    单链表

--- src/org/lhx/linkedList/SingleLinkedList.java
+++ src/org/lhx/linkedList/SingleLinkedList.java
@@ -21,6 +21,34 @@ public class SingleLinkedList {
         temp.next = heroNode;
     }
 
+    public void addByOrder(HeroNode heroNode){
+        HeroNode temp = head;
+        //添加的编号是否存在
+        boolean flag = false;
+        while (true){
+            //说明temp在链表的最后
+            if (temp.next == null){
+                break;
+            }
+            //位置找到，在temp后面插入
+            if (temp.next.getNo() > heroNode.getNo()){
+                break;
+            }else if (temp.next.getNo() == heroNode.getNo()){//希望添加的hero已经存在
+                //说明编号存在
+                flag = true;
+                break;
+            }
+            temp = temp.next;
+        }
+        //判断flag，为真则说明存在，不能添加
+        if (flag == true){
+            System.out.println("要插入的已经存在");
+        }else{
+            heroNode.next = temp.next;
+            temp.next = heroNode;
+        }
+    }
+
     public void list(){
         //判断链式是否为空
         if (head.next == null){
@@ -39,6 +67,31 @@ public class SingleLinkedList {
 
 class HeroNode{
     private int no;
+
+    public int getNo() {
+        return no;
+    }
+
+    public void setNo(int no) {
+        this.no = no;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
     private String name;
     private String nickname;
     public HeroNode next;
